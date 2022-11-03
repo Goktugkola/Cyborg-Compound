1d00fc010e3498071c2753be5497542902d7cd37
Göktuğ Kola <goktugkola@gmail.com>
2022-11-03 21:08:22 +0300

    hentaiiii
    hib

--- CikiletaFighters/Scenes/livil1.gd
+++ CikiletaFighters/Scenes/livil1.gd
@@ -3,6 +3,14 @@ extends Node2D
 func _ready():
 	pass # Replace with function body.
 func _physics_process(delta):
+	if($Player1/HealthBar.value <= 0):
+		$RichTextLabel.add_text("PLAYER 2 WON")
+		yield (get_tree().create_timer(10),"timeout")
+	if($Player2/HealthBar.value <= 0):
+		$RichTextLabel.add_text("PLAYER 1 WON")
+		yield (get_tree().create_timer(10),"timeout")
+	
+	
 	$Camera2D.transform.xform($Player1.position.x)
 	if($Player1/HealthBar.value <= 0 || $Player2/HealthBar.value <= 0):
 		get_tree().reload_current_scene()
