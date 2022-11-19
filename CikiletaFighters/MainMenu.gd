5c5a1e1dd2f82da1fc1a79cb74b1841d07ad7c80
Göktuğ Kola <goktugkola@gmail.com>
2022-11-19 22:18:33 +0300

    added main menu

--- CikiletaFighters/MainMenu.gd
+++ CikiletaFighters/MainMenu.gd
@@ -6,11 +6,16 @@ extends Control
 # var b = "text"
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+func _on_QUIT_pressed():
+	get_tree().quit()
+	pass # Replace with function body.
+
+
+func _on_PLAY_pressed():
+	get_tree().change_scene("res://Scenes/livil10.tscn")
+	pass # Replace with function body.
