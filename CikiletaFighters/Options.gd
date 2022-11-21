e9982f2bace908307309d4e7a6960a816c2222dd
Göktuğ Kola <goktugkola@gmail.com>
2022-11-21 15:24:38 +0300

    options

--- CikiletaFighters/Options.gd
+++ CikiletaFighters/Options.gd
@@ -6,11 +6,12 @@ extends Control
 # var b = "text"
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+func _on_CheckButton_toggled(button_pressed):
+	OS.set_window_fullscreen(!OS.window_fullscreen)
+	pass # Replace with function body.
