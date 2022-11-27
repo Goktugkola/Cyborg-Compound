36ceadc465a07ac1c2c9144329bbdcf61c8f63da
OzkanCetin <ozkancetin@posta.mu.edu.tr>
2022-11-27 17:14:48 +0300

    mermi olayı şimdilik bitti

--- CikiletaFighters/Scenes/bullet.gd
+++ CikiletaFighters/Scenes/bullet.gd
@@ -2,10 +2,19 @@ class_name Projectile
 extends Node2D
 var once : bool = true
 export var speed:= 1000.0
-export var lifetime:= 3.0
+export var lifetime:= 1.0
 var velocity:= Vector2()
 onready var timer:= $Timer
+onready var hitbox:= $hitbox_b
+onready var sprite:= $sprite_b
 onready var impact_detectorb:= $sprite_b/impactDetector_b
+
+func _ready():
+	set_as_toplevel(true)
+	timer.connect("timeout", self, "queue_free")
+	timer.start(lifetime)
+	
+	impact_detectorb.connect("body_entered",self, "_on_impact")
 
 func _physics_process(delta: float) -> void:
 	
@@ -13,6 +22,10 @@ func _physics_process(delta: float) -> void:
 		velocity = Vector2(speed , 0)
 		once = false
 	if G.p1_direction == false and once:
+		$sprite_b.flip_h = true
 		velocity = Vector2(-speed, 0)
 		once = false
 	self.translate(velocity * delta)
+
+func _on_impact(_body: Node) -> void:
+	queue_free()
--- CikiletaFighters/Scenes/bullet2.gd
+++ CikiletaFighters/Scenes/bullet2.gd
@@ -2,12 +2,21 @@ class_name Projectile2
 extends Node2D
 var once : bool = true
 export var speed:= 1000.0
-export var lifetime:= 3.0
+export var lifetime:= 1.0
 
 var velocity:= Vector2()
 
 onready var timer:= $Timer
+onready var hitbox:= $hitbox_b
+onready var sprite:= $sprite_b
 onready var impact_detectorb:= $sprite_b/impactDetector_b
+
+func _ready():
+	set_as_toplevel(true)
+	timer.connect("timeout", self, "queue_free")
+	timer.start(lifetime)
+	
+	impact_detectorb.connect("body_entered",self, "_on_impact")
 
 func _physics_process(delta: float) -> void:
 	
@@ -16,5 +25,9 @@ func _physics_process(delta: float) -> void:
 		once = false
 	if G.p2_direction == false and once:
 		velocity = Vector2(-speed, 0)
+		$sprite_b.flip_h = true
 		once = false
 	self.translate(velocity * delta)
+
+func _on_impact(_body: Node) -> void:
+	queue_free()
