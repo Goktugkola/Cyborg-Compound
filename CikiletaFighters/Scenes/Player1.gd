a453c2ec20cce4e8a38de64357fea1ab07a8bd19
OzkanCetin <ozkancetin@posta.mu.edu.tr>
2022-11-28 15:09:03 +0300

    a

--- CikiletaFighters/Scenes/Player1.gd
+++ CikiletaFighters/Scenes/Player1.gd
@@ -14,7 +14,7 @@ var knockbacktime = 0
 
 func _shoot():
 	var bullet =  bullet_path.instance()
-	
+	yield(get_tree(), "idle_frame")
 	get_parent().add_child(bullet)
 	bullet.position = $Node2D/Position2D.global_position
 func _ready():
@@ -92,10 +92,11 @@ func _physics_process(delta: float) -> void:
 			$DuckHurtBox/CollisionShape2D.set_deferred("disabled", true)
 
 func _on_Hurtbox_area_entered(_area):
+	yield(get_tree(), "idle_frame")
 	Health -=10
 	if G.p1_position.x < G.p2_position.x:
 		_velocity.x = 0
-		get_node(".").position.x -= (1 / ( Health + 1 ) * 20 ) 
+		get_node(".").position.x -= 20
 	else:
 		_velocity.x = 0
 		get_node(".").position.x += 20
