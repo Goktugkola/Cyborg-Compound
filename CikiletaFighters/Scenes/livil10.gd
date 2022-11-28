87772389e26b745db09c87f05e55ab9d4c0c3522
Göktuğ Kola <goktugkola@gmail.com>
2022-11-28 13:40:45 +0300

    Update livil10.gd

--- CikiletaFighters/Scenes/livil10.gd
+++ CikiletaFighters/Scenes/livil10.gd
@@ -2,12 +2,6 @@ extends Node2D
 
 
 var once : bool = true
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	$Livil1Ui/p1winstatus.add_text(str(G.p1wonstatus))
 	$Livil1Ui/p2winstatus.add_text(str(G.p2wonstatus))
